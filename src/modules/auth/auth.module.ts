5ea3e8e7adc22cef01f75f09cd720ad5ce1042e7
Alison Gabriel <odevalison@gmail.com>
2026-03-22 15:47:43 -0300

    fix: change deprecated secretOrPrivateKey on JwtModule.register

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,14 +5,14 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { LoginUseCase } from './use-cases/login.use-case';
 import { AuthController } from './controllers/auth.controller';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { SignupUseCase } from './use-cases/signup.use-case';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
-      secretOrPrivateKey: process.env.JWT_SECRET,
+      secret: process.env.JWT_SECRET,
       signOptions: {
         issuer: process.env.JWT_TOKEN_ISSUER,
         audience: process.env.JWT_TOKEN_AUDIENCE,
