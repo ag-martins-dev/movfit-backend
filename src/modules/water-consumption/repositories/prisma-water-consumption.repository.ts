c2c55a4dba200447b7abc730e1b8cbcf38a51235
Alison Gabriel <odevalison@gmail.com>
2026-04-19 01:57:17 -0300

    refactor: update WaterConsumption repository to extend BaseRepository

--- src/modules/water-consumption/repositories/prisma-water-consumption.repository.ts
+++ src/modules/water-consumption/repositories/prisma-water-consumption.repository.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common'
 import { WaterConsumption } from 'generated/prisma/client'
+import { BaseRepository } from 'src/common/repositories/base.repository'
+import { TransactionContextService } from 'src/common/services/transaction-context.service'
 import { PrismaService } from 'src/infra/database/prisma/prisma.service'
 import { GetWaterConsumptionHistoryRequest } from '../types/get-water-consumption-history.type'
 import { RegisterWaterConsumptionInput } from '../types/register-water-consumption.type'
 import { WaterConsumptionRepository } from './water-consumption.repository'
 
 @Injectable()
-export class PrismaWaterConsumptionRepository implements WaterConsumptionRepository {
-  constructor(private readonly prisma: PrismaService) {}
+export class PrismaWaterConsumptionRepository extends BaseRepository implements WaterConsumptionRepository {
+  constructor(
+    readonly prisma: PrismaService,
+    readonly transactionContext: TransactionContextService,
+  ) {
+    super(prisma, transactionContext)
+  }
 
-  async getHistory(
-    userId: string,
-    input: GetWaterConsumptionHistoryRequest,
-  ): Promise<WaterConsumption[]> {
-    const waterConsumptions = await this.prisma.waterConsumption.findMany({
+  async getHistory(userId: string, input: GetWaterConsumptionHistoryRequest): Promise<WaterConsumption[]> {
+    return await this.db.waterConsumption.findMany({
       where: {
         userId,
         dateOfConsumption: {
@@ -22,11 +26,10 @@ export class PrismaWaterConsumptionRepository implements WaterConsumptionReposit
         },
       },
     })
-    return waterConsumptions
   }
 
   async register(userId: string, input: RegisterWaterConsumptionInput): Promise<WaterConsumption> {
-    const waterConsumption = await this.prisma.$transaction(async (tx) => {
+    return await this.db.$transaction(async (tx) => {
       const waterConsumption = await tx.waterConsumption.create({
         data: {
           userId,
@@ -35,12 +38,13 @@ export class PrismaWaterConsumptionRepository implements WaterConsumptionReposit
           dateOfConsumption: input.dateOfConsumption,
         },
       })
+
       await tx.dailyWaterConsumption.update({
         where: { userId, id: input.dailyWaterConsumptionId },
         data: { consumedInMl: { increment: input.amountConsumedInMl } },
       })
+
       return waterConsumption
     })
-    return waterConsumption
   }
 }
