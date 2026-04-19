7d7dcd28f5cd1921fa1034689a868a780a57c51f
Alison Gabriel <odevalison@gmail.com>
2026-04-19 00:00:54 -0300

    feat: register FoodsModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './modules/auth/auth.module'
 import { DailyNutritionModule } from './modules/daily-nutrition/daily-nutrition.module'
 import { DailyWaterConsumptionModule } from './modules/daily-water-consumption/daily-water-consumption.module'
 import { DietsModule } from './modules/diets/diets.module'
+import { FoodsModule } from './modules/foods/foods.module'
 import { ProfileModule } from './modules/profile/profile.module'
 import { UsersModule } from './modules/users/users.module'
 import { WaterConsumptionHistoryModule } from './modules/water-consumption/water-consumption.module'
@@ -23,6 +24,7 @@ import { WorkoutConfigModule } from './modules/workout-config/workout-config.mod
     WorkoutConfigModule,
     ProfileModule,
     DietsModule,
+    FoodsModule,
   ],
   providers: [AppService],
 })
