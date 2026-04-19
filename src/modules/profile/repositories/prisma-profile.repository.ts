1fb54f7808c5b137df942d29758f74c142fe95a8
Alison Gabriel <odevalison@gmail.com>
2026-04-19 01:57:03 -0300

    refactor: update Profile repository to extend BaseRepository

--- src/modules/profile/repositories/prisma-profile.repository.ts
+++ src/modules/profile/repositories/prisma-profile.repository.ts
@@ -1,33 +1,37 @@
 import { Injectable } from '@nestjs/common'
 import { Profile } from 'generated/prisma/client'
+import { BaseRepository } from 'src/common/repositories/base.repository'
+import { TransactionContextService } from 'src/common/services/transaction-context.service'
 import { PrismaService } from 'src/infra/database/prisma/prisma.service'
 import { CompleteProfileRequest } from '../types/complete-profile.type'
 import { ProfileRepository } from './profile.repository'
 
 @Injectable()
-export class PrismaProfileRepository implements ProfileRepository {
-  constructor(private readonly prisma: PrismaService) {}
+export class PrismaProfileRepository extends BaseRepository implements ProfileRepository {
+  constructor(
+    readonly prisma: PrismaService,
+    readonly transactionContext: TransactionContextService,
+  ) {
+    super(prisma, transactionContext)
+  }
 
   async completeProfile(userId: string, input: CompleteProfileRequest) {
-    await this.prisma.$transaction([
-      this.prisma.profile.upsert({
+    await this.db.$transaction(async (tx) => {
+      tx.profile.upsert({
         where: { userId },
         create: { userId, ...input },
         update: input,
-      }),
-      this.prisma.user.update({
-        where: { id: userId },
+      })
+      tx.user.update({
+        where: { id: userId, isOnboardingCompleted: { equals: false } },
         data: { isOnboardingCompleted: true },
-      }),
-    ])
+      })
+    })
   }
+
   async getProfile(userId: string): Promise<Profile | null> {
-    const profile = await this.prisma.profile.findFirst({
+    return await this.db.profile.findFirst({
       where: { userId },
     })
-    if (!profile) {
-      return null
-    }
-    return profile
   }
 }
