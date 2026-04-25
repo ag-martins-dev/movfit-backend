52ee5f5c997fe043a3ecb196ed52d919f88874fd
Alison Gabriel <odevalison@gmail.com>
2026-04-24 21:05:06 -0300

    refactor: update workout config controller with standardized decorators

--- src/modules/workout-config/controller/workout-config.controller.ts
+++ src/modules/workout-config/controller/workout-config.controller.ts
@@ -1,25 +1,11 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Post,
-  UseGuards,
-  UseInterceptors,
-} from '@nestjs/common'
-import { ApiResponse } from '@nestjs/swagger'
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger'
 import { CurrentUser } from 'src/common/decorators/current-user.decorator'
-import { RequireProfile } from 'src/common/decorators/require-profile.decorator'
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard'
 import { OnboardingGuard } from 'src/common/guards/onboarding.guard'
-import { ProfileInterceptor } from 'src/common/interceptors/profile.interceptor'
 import type { AuthUser } from 'src/common/types/auth-user.types'
-import { GetWorkoutConfigResponseDto } from '../dtos/get-workout-config-response.dto'
-import {
-  RegisterWorkoutConfigRequestDto,
-  RegisterWorkoutConfigResponseDto,
-} from '../dtos/register-workout-config.dto'
+import { GetWorkoutConfigResponseDTO } from '../dtos/get-workout-config-response.dto'
+import { RegisterWorkoutConfigRequestDTO, RegisterWorkoutConfigResponseDTO } from '../dtos/register-workout-config.dto'
 import { GetWorkoutConfigUseCase } from '../use-cases/get-workout-config.use-case'
 import { RegisterWorkoutConfigUseCase } from '../use-cases/register-workout-config.use-case'
 
@@ -31,27 +17,16 @@ export class WorkoutConfigController {
     private readonly getWorkoutConfigUseCase: GetWorkoutConfigUseCase,
   ) {}
 
-  @RequireProfile()
-  @UseInterceptors(ProfileInterceptor)
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Get current user workout config.',
-    type: GetWorkoutConfigResponseDto,
-  })
   @Get()
-  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: GetWorkoutConfigResponseDTO })
   getWorkoutConfig(@CurrentUser() user: AuthUser) {
-    return this.getWorkoutConfigUseCase.execute({ userId: user.id })
+    return this.getWorkoutConfigUseCase.execute(user.id)
   }
 
-  @ApiResponse({
-    status: HttpStatus.CREATED,
-    description: 'Register a new user workout config.',
-    type: RegisterWorkoutConfigResponseDto,
-  })
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  registerWorkoutConfig(@Body() dto: RegisterWorkoutConfigRequestDto) {
-    return this.registerWorkoutConfigUseCase.execute(dto)
+  @ApiCreatedResponse({ type: RegisterWorkoutConfigResponseDTO })
+  registerWorkoutConfig(@Body() body: RegisterWorkoutConfigRequestDTO) {
+    return this.registerWorkoutConfigUseCase.execute(body)
   }
 }
