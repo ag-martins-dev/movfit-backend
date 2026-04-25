f5a2670b7233e4ea63a6da5ad0c3828d729e9f84
Alison Gabriel <odevalison@gmail.com>
2026-04-24 21:50:43 -0300

    refactor: simplify toPercentage helper function signature

--- src/common/helpers/to-percentage.helper.ts
+++ src/common/helpers/to-percentage.helper.ts
@@ -1,9 +1,4 @@
-type ToPercentageInputData = {
-  goal: number
-  current: number
-}
-
-export function toPercentage({ current, goal }: ToPercentageInputData): number {
+export function toPercentage(goal: number, current: number): number {
   if (goal === 0) return 0
   return Math.floor(Math.min((current / goal) * 100, 100))
 }
