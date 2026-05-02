35c853b5dd074a19e8f631da42b7ec884b8a342a
Alison Gabriel <odevalison@gmail.com>
2026-05-01 23:44:34 -0300

    refactor: move BaseRepository to infra folder

---
+++ src/infra/database/repositories/base.repository.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/infra/database/prisma/prisma.service'
+import { TransactionContextService } from '../../../common/services/transaction-context.service'
+
+@Injectable()
+export abstract class BaseRepository {
+  constructor(
+    protected readonly prisma: PrismaService,
+    protected readonly transactionContext: TransactionContextService,
+  ) {}
+
+  protected get db() {
+    return this.transactionContext.get() ?? this.prisma
+  }
+}
