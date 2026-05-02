cf560fc491dd5e7ba705103b087837efe48cc473
Alison Gabriel <odevalison@gmail.com>
2026-05-02 00:09:31 -0300

    chore: add Scalar API reference

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,40 @@ import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
 
-  app.enableCors({ origin: '*', methods: '*' })
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  })
 
   app.enableVersioning({ type: VersioningType.URI })
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
 
   const config = new DocumentBuilder()
-    .setTitle('API - Movfit App')
-    .setDescription('API - Movfit App')
-    .setVersion('1.0.0')
+    .setTitle('Movfit API')
+    .setDescription('Movfit API documentation')
+    .setVersion('1.0')
+    .addTag('movfit-api')
     .addBearerAuth()
     .build()
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('docs', app, documentFactory)
+  const document = SwaggerModule.createDocument(app, config)
+
+  app.use(
+    '/docs',
+    apiReference({
+      theme: 'purple',
+      content: document,
+      withFastify: true,
+    }),
+  )
 
   await app.register(() => helmet)
   await app.listen(process.env.PORT ?? 4949)
